81dd05d6a7531a97720e50a15495ce6928c083d6
muzaffarshaikh <52539616+muzaffarshaikh@users.noreply.github.com>
2019-07-11 12:18:08 +0530

    Add files via upload

---
+++ 11-7-2019/fcfs.c
@@ -0,0 +1,7 @@
+//program to calculate first come first serve algo
+//input as number of jobs and arrival time of jobs
+//and at the end it is to calculate avg waiting time & avg response time
+#include<stdio.h>
+
+int i,j;
+
