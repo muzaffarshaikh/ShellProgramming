b7752b4ee6d3d472322e20160d8dede603814ea6
muzaffarshaikh <52539616+muzaffarshaikh@users.noreply.github.com>
2020-11-11 19:46:01 +0530

    Another Intermediate Commit

--- Yet-To-Arrange/11-7-2019/cmd_line_args.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-FILE *fp; fp = fopen("new.txt","w+");
-fputs("Master of Science in Information Technology",fp);
-fseek(fp,3,SEEK_SET);
-fputs("This is me", fp);
-fclose(fp);
-return(0);
-}
--- Yet-To-Arrange/11-7-2019/fcfs.c
+++
@@ -1,7 +0,0 @@
-//program to calculate first come first serve algo
-//input as number of jobs and arrival time of jobs
-//and at the end it is to calculate avg waiting time & avg response time
-#include<stdio.h>
-
-int i,j;
-
